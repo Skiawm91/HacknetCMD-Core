96daffced0e644e7d41e88967ac076d77615ba2f
Skiawm91 <guoy2674@outlook.com>
2025-12-13 22:35:38 +0800

    No commit

--- include/misc.h
+++ include/misc.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <string>
+#include <vector>
 class Misc {
 public:
     std::string toLangName(const int code);
+    struct Node_Modify {
+        /* void rename(const std::vector<std::string>& path, const std::string&);  UNFINISH */
+        void parameter(const std::string& name, const std::string& value);
+    };
 };
